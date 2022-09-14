4a19edc4b422dafc32396f9135033e61218d1aae
Ninjagoboy <111666061+Ninjagoboy@users.noreply.github.com>
2022-09-14 18:35:38 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+console.log("my first console project")
+var fname="Vera"
+var lname="O'brien"
+var fullname=fname.concat(lname)
+console.log("first name: "+fname)
+console.log("last name: "+lname)
+console.log("fullname:" +fullname)
+var a=200
+var b=250
+var add=a+b
+console.log("num1="+a)
+console.log("num2="+b )
+console.log("sum="+add )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
